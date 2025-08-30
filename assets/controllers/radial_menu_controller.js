06d2974febb41ab5a01df67c490f1b5aeab98ca5
Emmanuel SMITH <emmanuel.smith@jagfx.fr>
2025-08-30 10:56:29 +0200

    :lipstick: add element + label to radial menu

--- assets/controllers/radial_menu_controller.js
+++ assets/controllers/radial_menu_controller.js
@@ -8,6 +8,37 @@ export default class extends Controller {
         offset: {type: Number, defaultValue: 0}
     }
 
+    calculateTitleOffset(angle) {
+        const absAngle = Math.abs(angle);
+        const normalizedAngle = Math.min(90, Math.max(0, absAngle));
+
+            const t = (normalizedAngle - 45) / 45;
+            return -0.5 - (0.5 * t);
+    }
+
+    calculateStackOffset(angle) {
+        const normalizedAngle = MAX_ANGLE - angle;
+
+        if (normalizedAngle >= 90) {
+            return 0;
+        }
+
+        if (normalizedAngle >= 60) {
+            const intensity = (normalizedAngle - 60) / 30;
+            return -0.2 * intensity;
+        }
+
+        if (normalizedAngle < 60) {
+            const intensity = (60 - normalizedAngle) / 60;
+            const majorSpacing = -.5;
+            const stackEffect = -3.3 * intensity * intensity;
+
+            return majorSpacing + stackEffect;
+        }
+
+        return 0;
+    }
+
     connect() {
         const maxAngle = this.maxAngleValue;
         const offset = this.offsetValue;
@@ -29,6 +60,21 @@ export default class extends Controller {
             item.style.setProperty('--angle', `${angle}deg`);
             item.style.setProperty('--angle-correction', `${angleCorrection}deg`);
             item.style.setProperty('--angle-pointer', `${anglePointer}deg`);
+
+            const titleOffset = this.calculateTitleOffset(angleCorrection);
+            const stackOffset = this.calculateStackOffset(Math.abs(angle));
+
+            item.style.setProperty('--title-offset', `${titleOffset}rem`);
+
+            const title = item.title ?? '';
+            const titleElement = document.createElement('small');
+            titleElement.classList.add('title', 'badge');
+            titleElement.textContent = title;
+            titleElement.style.setProperty('--title-offset',
+                stackOffset === 0 ? 'initial' : `${stackOffset}rem`
+            );
+
+            item.appendChild(titleElement);
         });
     }
 }
